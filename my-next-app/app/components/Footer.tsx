9ec16a34f1e04514dec5b7c780d3def346dec18d
Rutujeet28 <rutujeetfalmare28@gmail.com>
2025-11-22 00:49:44 -0600

    Updated the required structure

--- my-next-app/app/components/Footer.tsx
+++ my-next-app/app/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
+
     <footer className="w-full bg-white text-black py-4 text-center mt-10 h-16 flex items-center justify-center shadow-inner">
       <p className="text-sm font-bold">© 2025 Ayurveda. All rights reserved.</p>
     </footer>
+    
   );
 }
